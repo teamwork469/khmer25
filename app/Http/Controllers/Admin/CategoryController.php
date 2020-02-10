933410cd3e170cde0f5efcf8daa3103d838b80fc
teamwork469 <59301419+teamwork469@users.noreply.github.com>
2020-02-11 00:02:54 +0700

    Update CategoryController.php

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -73,7 +73,7 @@ class CategoryController extends CrudController {
       $this->crud->addField([  // Select
         'label' => "Main Category",
         'type' => 'select2',
-        'name' => 'id', // the db column for the foreign key
+        'name' => 'main_cat_id', // the db column for the foreign key
         'entity' => 'main_category', // the method that defines the relationship in your Model
         'attribute' => 'main_name', // foreign key attribute that is shown to user
         'model' => "App\Models\MainCategory",
