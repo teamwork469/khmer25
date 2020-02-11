3d9f6fe5b5f204e0dbe63f4877af34ce11f42300
teamwork469 <59301419+teamwork469@users.noreply.github.com>
2020-02-11 23:25:28 +0700

    Update custom.php

--- routes/backpack/custom.php
+++ routes/backpack/custom.php
@@ -13,4 +13,5 @@ Route::group([
 ], function () { // custom admin routes
     Route::crud('category', 'CategoryController');
     Route::crud('maincategory', 'MainCategoryController');
+    Route::crud('gallery', 'GalleryImageController');
 }); // this should be the absolute last line of this file
