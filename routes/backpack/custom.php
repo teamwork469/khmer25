30f792a569b39c1c7097499b9837c411a05b5322
teamwork469 <59301419+teamwork469@users.noreply.github.com>
2020-02-11 22:46:34 +0700

    Update custom.php

--- routes/backpack/custom.php
+++ routes/backpack/custom.php
@@ -14,13 +14,3 @@ Route::group([
     Route::crud('category', 'CategoryController');
     Route::crud('maincategory', 'MainCategoryController');
 }); // this should be the absolute last line of this file
-
-
-
-Route::group([
-    'prefix'     => config('backpack.base.route_prefix', 'admin'),
-    'middleware' => ['web', config('backpack.base.middleware_key', 'admin')],
-    'namespace'  => 'App\Http\Controllers\Admin',
-], function () { // custom admin routes
-    
-}); // this should be the absolute last line of this file
