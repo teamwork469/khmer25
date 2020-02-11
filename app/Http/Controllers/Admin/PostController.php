8226b4b1102704f919f358b1c79a1a038fe96fe4
teamwork469 <59301419+teamwork469@users.noreply.github.com>
2020-02-11 23:45:24 +0700

    Update PostController.php

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -1,10 +1,50 @@
-<?php
+<?php namespace App\Http\Controllers\Admin;
 
-namespace App\Http\Controllers;
+use Backpack\CRUD\app\Http\Controllers\CrudController;
 
-use Illuminate\Http\Request;
 
-class PostController extends CURDController
-{
-    //
+class MainCategoryController extends CrudController {
+
+  use \Backpack\CRUD\app\Http\Controllers\Operations\ListOperation;
+  use \Backpack\CRUD\app\Http\Controllers\Operations\ShowOperation;
+  use \Backpack\CRUD\app\Http\Controllers\Operations\CreateOperation;
+  use \Backpack\CRUD\app\Http\Controllers\Operations\UpdateOperation;
+  use \Backpack\CRUD\app\Http\Controllers\Operations\DeleteOperation;
+
+  public function setup() 
+  {
+      $this->crud->setModel("App\Models\MainCategory");
+      $this->crud->setRoute("admin/maincategory");
+      $this->crud->setEntityNameStrings('Main Category', 'Main Category');
+
+      $this->crud->enableExportButtons();
+      //$this->crud->setActionsColumnPriority(10000);
+  }
+
+  public function setupListOperation()
+  {
+    // $this->crud->addColumn([
+    //   'name' => 'main_category_name', // The db column name
+    //   'label' => "Main Category", // Table column heading
+    //   'type' => 'Text'
+    // ]);
+   
+
+  }
+
+  public function setupCreateOperation()
+  {
+      //$this->crud->setValidation(TagCrudRequest::class);
+
+    //   $this->crud->addField([
+    //     'name' => 'main_category_name',
+    //     'type' => 'text',
+    //     'label' => "Main Category"
+    //   ]);
+  }
+
+  public function setupUpdateOperation()
+  {
+      $this->setupCreateOperation();
+  }
 }
