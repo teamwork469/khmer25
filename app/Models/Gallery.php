bae260a221975d2a27bc6a5f450cc72dc7d7ce34
teamwork469 <59301419+teamwork469@users.noreply.github.com>
2020-02-11 23:22:53 +0700

    Update Gallery.php

--- app/Models/Gallery.php
+++ app/Models/Gallery.php
@@ -1,10 +1,63 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Backpack\CRUD\app\Models\Traits\CrudTrait;
+use Illuminate\Database\Eloquent\Models;
 
-class Gallery extends Model
-{
-    //
+
+class Category extends Model {
+
+  use CrudTrait;
+
+  /*
+  |--------------------------------------------------------------------------
+  | GLOBAL VARIABLES
+  |--------------------------------------------------------------------------
+  */
+
+  protected $table = 'category';
+  protected $primaryKey = 'category_id';
+  // public $timestamps = false;
+  protected $guarded = ['category_id'];
+
+  protected $fillable = ['main_category_id','category_name','description','created_at','updated_at'];
+  // protected $hidden = [];
+  // protected $dates = [];
+
+  /*
+  |--------------------------------------------------------------------------
+  | FUNCTIONS
+  |--------------------------------------------------------------------------
+  */
+
+
+  /*
+  |--------------------------------------------------------------------------
+  | RELATIONS
+  |--------------------------------------------------------------------------
+  */
+    public function main_category()
+    {
+      return $this->belongsTo('App\Models\MainCategory','main_category_id');
+    }
+
+  /*
+  |--------------------------------------------------------------------------
+  | SCOPES
+  |--------------------------------------------------------------------------
+  */
+
+  /*
+  |--------------------------------------------------------------------------
+  | ACCESORS
+  |--------------------------------------------------------------------------
+  */
+
+  /*
+  |--------------------------------------------------------------------------
+  | MUTATORS
+  |--------------------------------------------------------------------------
+  */
 }
