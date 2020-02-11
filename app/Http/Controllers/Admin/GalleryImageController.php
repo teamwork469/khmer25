1710e870ad4554bf659cd607942803b229e052a8
teamwork469 <59301419+teamwork469@users.noreply.github.com>
2020-02-11 23:28:18 +0700

    Update GalleryImageController.php

--- app/Http/Controllers/Admin/GalleryImageController.php
+++ app/Http/Controllers/Admin/GalleryImageController.php
@@ -3,7 +3,7 @@
 use Backpack\CRUD\app\Http\Controllers\CrudController;
 
 
-class GalleryImageController extends CrudController {
+class CategoryController extends CrudController {
 
   use \Backpack\CRUD\app\Http\Controllers\Operations\ListOperation;
   use \Backpack\CRUD\app\Http\Controllers\Operations\ShowOperation;
@@ -11,7 +11,44 @@ class GalleryImageController extends CrudController {
   use \Backpack\CRUD\app\Http\Controllers\Operations\UpdateOperation;
   use \Backpack\CRUD\app\Http\Controllers\Operations\DeleteOperation;
   use \Backpack\CRUD\app\Http\Controllers\Operations\CloneOperation;
-    //
 
+  public function setup() 
+  {
+      $this->crud->setModel("App\Models\Category");
+      $this->crud->setRoute("admin/category");
+      $this->crud->setEntityNameStrings('category', 'category');
 
+      $this->crud->enableExportButtons();
+      //$this->crud->setActionsColumnPriority(10000);
+      //$this->crud->enableBulkActions();
+  }
+
+  public function setupListOperation()
+  {
+    // $this->crud->addColumn([
+    //   'name' => 'category_name', // The db column name
+    //   'label' => "Category", // Table column heading
+    //   'type' => 'Text'
+    // ]);
+  
+  }
+
+  public function setupCreateOperation()
+  {
+      //$this->crud->setValidation(TagCrudRequest::class);
+
+    //   $this->crud->addField([
+    //     'name' => 'category_name',
+    //     'type' => 'text',
+    //     'label' => "Category",
+    //   ]);
+
+      
+ 
+  }
+
+  public function setupUpdateOperation()
+  {
+      $this->setupCreateOperation();
+  }
 }
