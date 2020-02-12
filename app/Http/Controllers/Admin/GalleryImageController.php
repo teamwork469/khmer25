8507e56beacabc4ffbbacbf3d9d16c4ce03e8504
teamwork469 <59301419+teamwork469@users.noreply.github.com>
2020-02-12 11:42:31 +0700

    Update GalleryImageController.php

--- app/Http/Controllers/Admin/GalleryImageController.php
+++ app/Http/Controllers/Admin/GalleryImageController.php
@@ -132,6 +132,18 @@ $this->crud->addField([   // radio
 ]);
 
 
+$this->crud->addField([   // select_grouped
+  'label' => 'Articles grouped by categories',
+  'type' => 'select_grouped', //https://github.com/Laravel-Backpack/CRUD/issues/502
+  'name' => 'article_id',
+  'entity' => 'article',
+  'attribute' => 'title',
+  'group_by' => 'category', // the relationship to entity you want to use for grouping
+  'group_by_attribute' => 'name', // the attribute on related model, that you want shown
+  'group_by_relationship_back' => 'articles', // relationship from related model back to this model
+]);
+
+
 
 $this->crud->addField([   // Color
   'name' => 'background_color',
