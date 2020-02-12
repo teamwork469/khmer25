3a9dc36f3c4c63841b9f8bb7bbf449821b59a7f4
teamwork469 <59301419+teamwork469@users.noreply.github.com>
2020-02-12 14:41:14 +0700

    Update CategoryController.php

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -26,6 +26,43 @@ class CategoryController extends CrudController {
 
   public function setupListOperation()
   {
+    // simple filter
+    $this->crud->addFilter([
+      'type' => 'text',
+      'name' => 'category_name',
+      'label'=> 'Category Name'
+    ], 
+    false, 
+    function($value) { // if the filter is active
+       $this->crud->addClause('where', 'category_name', 'LIKE', "%$value%");
+    });
+
+    // date filter
+// $this->crud->addFilter([
+//   'type'  => 'date',
+//   'name'  => 'created_at',
+//   'label' => 'Record Date'
+// ],
+//   false,
+//   function ($value) { // if the filter is active, apply these constraints
+//       $this->crud->addClause('where', 'created_at', $value);
+//   });
+
+$this->crud->addFilter([
+  'type'  => 'date_range',
+  'name'  => 'created_at',
+  'label' => 'Date range'
+],
+  false,
+  function ($value) { // if the filter is active, apply these constraints
+      $dates = json_decode($value);
+      $this->crud->addClause('where', 'created_at', '>=', $dates->created_at);
+      $this->crud->addClause('where', 'updated_at', '<=', $dates->created_at . ' 23:59:59');
+  });
+
+
+
+
     $this->crud->addColumn([
       'name' => 'category_name', // The db column name
       'label' => "Category", // Table column heading
