7116d28254fda3afc105c592ccc96e216255f27b
teamwork469 <59301419+teamwork469@users.noreply.github.com>
2020-02-12 13:28:14 +0700

    Update GalleryImageController.php

--- app/Http/Controllers/Admin/GalleryImageController.php
+++ app/Http/Controllers/Admin/GalleryImageController.php
@@ -51,97 +51,23 @@ class GalleryImageController extends CrudController {
   {
       //$this->crud->setValidation(TagCrudRequest::class);
 
-      // $this->crud->addField([
-      //   'name' => 'gallery_name',
-      //   'type' => 'text',
-      //   'label' => "Image",
-      // ]);
+      $this->crud->addField([
+        'name' => 'gallery_name',
+        'type' => 'text',
+        'label' => "Image",
+      ]);
 
-      // $this->crud->addField([
-      //   'name'=>'created_at',
-      //   'type'=>'date',
-      //   'label'=>'create_at',
-      // ]);
+      $this->crud->addField([
+        'name'=>'created_at',
+        'type'=>'date',
+        'label'=>'create_at',
+      ]);
 
-      // $this->crud->addField([
-      //   'name'=>'updated_at',
-      //   'type'=>'date',
-      //   'label'=>'updated_at',
-      // ]);
-
-      $this->crud->addField([   // Address
-        'name' => 'file',
-        'label' => 'Image',
-        'type' => 'browse_multiple',
-        // optional
-        'store_as_json' => true
-    ]);
-    // base64_image
-$this->crud->addField([
-  'label'     => 'MainCategory',
-  'type'      => 'checklist',
-  'name'      => 'main_category_id',
-  'entity'    => 'main_category',
-  'attribute' => 'main_category_name',
-  'model'     => "App\Models\MainCategory",
-  'pivot'     => true,
-]);
-    // select_from_array
-$this->crud->addField([
-  'name' => 'select_from_array',
-  'label' => "Select from array",
-  'type' => 'select_from_array',
-  'options' => ['one' => 'One', 'two' => 'Two', 'three' => 'Three'],
-  'allows_null' => false,
-  'allows_multiple' => true,
-  'tab' => 'Tab name here',
-]);
-
-$this->crud->addField([   // icon_picker
-  'label' => "Icon",
-  'name' => 'icon',
-  'type' => 'icon_picker',
-  'iconset' => 'fontawesome' // options: fontawesome, glyphicon, ionicon, weathericon, mapicon, octicon, typicon, elusiveicon, materialdesign
-]);
-
-// image
-$this->crud->addField([
-  'label' => "Profile Image",
-  'name' => "image",
-  'type' => 'image',
-  'upload' => true,
-  'crop' => true, // set to true to allow cropping, false to disable
-  'aspect_ratio' => 1, // ommit or set to 0 to allow any aspect ratio
-  // 'disk' => 's3_bucket', // in case you need to show images from a different disk
-  // 'prefix' => 'uploads/images/profile_pictures/' // in case your db value is only the file name (no path), you can use this to prepend your path to the image src (in HTML), before it's shown to the user;
-]);
-
-
-$this->crud->addField([   // radio
-  'name'        => 'status', // the name of the db column
-  'label'       => 'Status', // the input label
-  'type'        => 'radio',
-  'options'     => [
-      // the key will be stored in the db, the value will be shown as label; 
-      0 => "Draft",
-      1 => "Published",
-      2 => "Private"
-  ],
-  // optional
-  //'inline'      => false, // show the radios all on the same line?
-]);
-
-
-
-
-
-
-$this->crud->addField([   // Color
-  'name' => 'background_color',
-  'label' => 'Background Color',
-  'type' => 'color_picker',
-  'default' => '#000000',
-]);
+      $this->crud->addField([
+        'name'=>'updated_at',
+        'type'=>'date',
+        'label'=>'updated_at',
+      ]);
 
 
   }
