8d1c008957ad035d9132f203f2109066d691e3f2
teamwork469 <59301419+teamwork469@users.noreply.github.com>
2020-02-13 14:07:12 +0700

    Update PostDetailController.php

--- app/Http/Controllers/Admin/PostDetailController.php
+++ app/Http/Controllers/Admin/PostDetailController.php
@@ -1,10 +1,40 @@
-<?php
+<?php namespace App\Http\Controllers\Admin;
 
-namespace App\Http\Controllers;
+use Backpack\CRUD\app\Http\Controllers\CrudController;
+use App\Http\Requests\TagRequest;
 
-use Illuminate\Http\Request;
+class PostDetailController extends CrudController {
 
-class PostDetailController extends Controller
-{
-    //
+  use \Backpack\CRUD\app\Http\Controllers\Operations\ListOperation;
+  use \Backpack\CRUD\app\Http\Controllers\Operations\CreateOperation;
+  use \Backpack\CRUD\app\Http\Controllers\Operations\UpdateOperation;
+  use \Backpack\CRUD\app\Http\Controllers\Operations\DeleteOperation;
+  use \Backpack\CRUD\app\Http\Controllers\Operations\ShowOperation;
+
+  public function setup() 
+  {
+      $this->crud->setModel("App\Models\Tag");
+      $this->crud->setRoute("admin/tag");
+      $this->crud->setEntityNameStrings('tag', 'tags');
+  }
+
+  protected function setupListOperation()
+  {
+      // TODO: remove setFromDb() and manually define Columns, maybe Filters
+      $this->crud->setFromDb();
+  }
+
+  protected function setupCreateOperation()
+  {
+      $this->crud->setValidation(TagRequest::class);
+
+      // TODO: remove setFromDb() and manually define Fields
+      $this->crud->setFromDb();
+  }
+
+  protected function setupUpdateOperation()
+  {
+      $this->setupCreateOperation();
+  }
+}
 }
